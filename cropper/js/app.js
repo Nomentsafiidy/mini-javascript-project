edd5388bd9228ca4ea28a56da3a71e3ec39a38ae
Nomentsafidy <mnomentsafidy@gmail.com>
2021-08-12 16:48:40 +0300

    feat define cropper

--- cropper/js/app.js
+++ cropper/js/app.js
@@ -1,1 +1,34 @@
-alert('okok');
+const fileUpload = document.querySelector('#fileUpload');
+const imageToCroppeContainer = document.querySelector('#imageToCroppeContainer');
+const previewCropped = document.querySelector('#previewCropped');
+const imageCroppedContainer = document.querySelector('#imageCroppedContainer');
+
+fileUpload.addEventListener('change', fileChange);
+document.querySelector('#croppedBtn').addEventListener('click', cropped);
+
+let cropper;
+
+/**
+ *
+ * @param {Event} e
+ */
+function fileChange(e) {
+    const file = e.target.files[0];
+    const imageToCropped = new Image();
+    imageToCropped.src = URL.createObjectURL(file);
+    imageToCroppeContainer.appendChild(imageToCropped);
+    cropper = new Cropper(imageToCropped, {
+        aspectRatio: 4 / 3,
+        crop(event) {
+            const canvas = this.cropper.getCroppedCanvas();
+            previewCropped.src = canvas.toDataURL('image/png');
+        },
+    });
+}
+
+function cropped(e) {
+    const canvas = cropper.getCroppedCanvas();
+    const image = new Image();
+    image.src = canvas.toDataURL('image/png');
+    imageCroppedContainer.appendChild(image);
+}
